921e7fcec7ad4281f038391a92708ea14c90e4d5
liliangquan <liliangquan@jd.com>
2018-05-18 23:51:04 +0800

    feat: fix

---
+++ vue-demo1/src/components/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+     hello {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  props: {
+    name: [String, Object]
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-demo1/src/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="index">
+    index {{ msg }}-{{count}}
+    <test :name="name"/>
+  </div>
+</template>
+
+<script>
+import test from '../components/test.vue'
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: '测试组件通信',
+      name: 'xiaoming'
+    }
+  },
+  created () {
+
+  },
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
+  mounted () {
+  },
+  components: {
+    test
+  }
+}
+</script>
+
+<style>
+  .index {
+
+  }
+</style>
